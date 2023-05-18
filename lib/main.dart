d49c27690f9a9228b9cf3d47f7d2048b4c307f53
amohkaranja <amohdkaranja@gmail.com>
2023-05-18 04:49:46 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:checkin/screens/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  MaterialColor buildMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Checkin',
+      theme: ThemeData(
+        primarySwatch: buildMaterialColor(const Color(0xff008346)),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: const Color(0xff00836F)),
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+  
